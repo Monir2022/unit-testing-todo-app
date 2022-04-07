d95ca40a801ad0968fa7c4d4ed54b29ac60901d8
Monir <akhandbuet@gmail.com>
2022-04-07 14:24:52 +0200

    Test at app has passed

--- src/screens/ShoppingScreen.jsx
+++ src/screens/ShoppingScreen.jsx
@@ -18,7 +18,7 @@ export default function ShoppingScreen() {
     setList(clonedList);
   }
   return (
-    <div data-testid="normalScreen" className="normal-screen">
+    <div data-testid ="shopping-Screen" className="normal-screen">
       <Sorter />
       <ShoppingList list={pendingList} editList={editList} />
       <ButtonAddItem />
