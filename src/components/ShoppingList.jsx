183149a72f2e71a8adb9d3af63d5a06b8cf5b6ea
Monir <akhandbuet@gmail.com>
2022-04-07 13:14:56 +0200

    font has fixed

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,7 +1,9 @@
 import ShoppingItem from "./ShoppingItem";
 
 export default function ShoppingList({list, editList}) {
+
   const items = list.map((item) => <ShoppingItem key={item.id} item={item} editList={editList} />);
+  
   return (
     <div>
       {list.length > 0 ? items : <p>No items to show!</p>}
