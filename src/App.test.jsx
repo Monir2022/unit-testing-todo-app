0ae87241a65a5303f66fe54e62159ced7f2c1771
Monir <akhandbuet@gmail.com>
2022-04-07 14:24:29 +0200

    First test has passed

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,53 @@
-import { render, screen } from '@testing-library/react';
+import { getAllByTestId, render, screen } from '@testing-library/react';
+import { RecoilRoot } from "recoil";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Expected to show the Welcome screen if the todo list is empty", () => {
+  // Arrange
+  render(
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  );
+  const data = [];
+  Storage.prototype.getItem = jest.fn(() => JSON.stringify(data));  
+
+  // Act
+  const textElement = screen.queryByText(/eika shopping list/i);
+
+//Assert
+  expect(textElement).toBeInTheDocument();
 });
+
+
+test("Expected to show the shopingscreen if the todo list has one or more items", () => {
+
+  // Arrange
+  render(
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>)
+
+  const item = {
+    id: 1,
+    name: "Table",
+    price: "100",
+    showAcquired: false,
+  };
+  const data = [item];
+
+  Storage.prototype.getItem = jest.fn(() => JSON.stringify(data));
+
+  
+
+  // Act
+ 
+  const textElement = screen.queryByText(/sort by:/i);
+
+  // Assert
+  expect(textElement).toBeInTheDoccument();
+});
+
+
+
+
