77f127adbd23538d35a853716420ef60d7e0ee2c
Monir <akhandbuet@gmail.com>
2022-04-08 17:32:44 +0200

    Some modification

--- src/scripts/validateName.test.jsx
+++ src/scripts/validateName.test.jsx
@@ -1,31 +1,40 @@
 import validateName from "./validatename";
 
 test("Should pass if everything is ok", () => {
+//Arrange
     const usedValue = "Table";
     const error = "You need a item name to start";
-  
+
+//Acts  
     const result = validateName(usedValue);
-  
+    
+ //Assert 
     expect(result.data).toBe(usedValue);
     expect(result.error).toBe(" ");
   });
 
 test("Should fails if text field is empty", () => {
+//Arrange
   const usedValue = " ";
   const error = "You need a item name to start";
 
+//Acts
   const result = validateName(usedValue);
 
+//Assert
   expect(result.data).toBe(" ");
   expect(result.error).toBe(error);
 });
 
 test("Should fails if text is only the empty string", () => {
+//Arrange
   const usedValue = "           ";
   const error = "You need a item name to start";
 
+//Acts
   const result = validateName(usedValue);
 
+//Assert
   expect(result.data).toBe(" ");
   expect(result.error).toBe(error);
 });
--- src/scripts/validatePrice.test.jsx
+++ src/scripts/validatePrice.test.jsx
@@ -1,41 +1,53 @@
 import validatePrice from "./validateprice";
 
 test("Should pass if value is positive", () => {
+//Arrange
   const usedValue = "100";
   const error = " ";
 
+//Acts
   const result = validatePrice(usedValue);
 
+//Assert
   expect(result.data).toBe(100);
   expect(result.error).toBe(" ");
 });
 
 test("Should fail if value is empty", () => {
+//Arrange
   const usedValue = " ";
   const error = "Price must be greater than zero";
 
+//Acts
   const result = validatePrice(usedValue);
 
+//Assert
   expect(result.data).toBe(" ");
   expect(result.error).toBe(error);
 });
 
 test("Should fail if value is negative", () => {
+//Arrange
   const usedValue = "-100";
   const error = "Price must be greater than zero";
 
+//Acts
   const result = validatePrice(usedValue);
 
+//Assert
   expect(result.data).toBe(" ");
   expect(result.error).toBe(error);
 });
 
 test("Should fail if value is zero", () => {
+//Arrange
   const usedValue = "0";
   const error = "Price must be greater than zero";
 
+//Acts
   const result = validatePrice(usedValue);
-
+  
+//Assert
   expect(result.data).toBe(" ");
   expect(result.error).toBe(error);
 });
